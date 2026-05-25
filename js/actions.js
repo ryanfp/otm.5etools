c728e42b969a0a8e115bacf855b2d68cc4e690b4
ryanfp <71905151+ryanfp@users.noreply.github.com>
2026-05-25 00:49:41 -0400

    fix(build): fixed bug that was preventing bestiary and actions pages from loading

--- js/actions.js
+++ js/actions.js
@@ -20,8 +20,8 @@ class ActionsSublistManager extends SublistManager {
 		const time = it.time ? it.time.map(tm => PageFilterActions.getTimeText(tm)).join("/") : "\u2014";
 		const cellsText = [it.name, time];
 
-		const $ele = $(`<div class="lst__row lst__row--sublist ve-flex-col">
-			<a href="#${hash}" class="lst__row-border lst__row-inner">
+		const ele = ee`<div class="ve-lst__row ve-lst__row--sublist ve-flex-col">
+			<a href="#${hash}" class="ve-lst__row-border ve-lst__row-inner">
 				${this.constructor._getRowCellsHtml({values: cellsText})}
 			</a>
 		</div>`
@@ -76,11 +76,11 @@ class ActionsPage extends ListPage {
 		const hash = UrlUtil.autoEncodeHash(it);
 		const time = it.time ? it.time.map(tm => PageFilterActions.getTimeText(tm)).join("/") : "\u2014";
 
-		eleLi.innerHTML = `<a href="#${hash}" class="lst__row-border lst__row-inner">
-			<span class="ve-col-0-3 px-0 ve-flex-vh-center lst__btn-toggle-expand ve-self-flex-stretch no-select">[+]</span>
-			<span class="ve-col-5-7 px-1 bold">${it.name}</span>
-			<span class="ve-col-4 px-1 ve-text-center">${time}</span>
-			<span class="ve-col-2 ve-text-center ${Parser.sourceJsonToSourceClassname(it.source)} pl-1 pr-0" title="${Parser.sourceJsonToFull(it.source)}" ${Parser.sourceJsonToStyle(it.source)}>${source}</span>
+		eleLi.innerHTML = `<a href="#${hash}" class="ve-lst__row-border ve-lst__row-inner">
+			<span class="ve-col-0-3 ve-px-0 ve-flex-vh-center ve-lst__btn-toggle-expand ve-self-flex-stretch ve-no-select">[+]</span>
+			<span class="ve-col-5-7 ve-px-1 ve-bold">${it.name}</span>
+			<span class="ve-col-4 ve-px-1 ve-text-center">${time}</span>
+			<span class="ve-col-2 ve-text-center ${Parser.sourceJsonToSourceClassname(it.source)} ve-pl-1 ve-pr-0" title="${Parser.sourceJsonToFull(it.source)}">${source}</span>
 		</a>
 		<div class="ve-flex ve-hidden ve-relative ve-accordion__wrp-preview">
 			<div class="ve-vr-0 ve-absolute ve-accordion__vr-preview"></div>
